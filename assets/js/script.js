889f7bdcd5812696f72922a6af7d040ccb4d3eea
wygrajr <wycliffe.graftonjr@gmail.com>
2023-05-23 13:02:34 -0400

    Renamed IDs in HTML. Added jquery and JSON script in HTML

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var key="2c5bcf8987116e3df22f7b64c6a46515";
+
+
+
+var days=[$("#today"),$("#dayNx1"),$("#dayNx2"),$("#dayNx3"),$("#dayNx4"),$("#dayNx5")]
+var location=[$("#location"),$("#dateNx1"),$("#dateNx2"),$("#dateNx3"),$("#dateNx4"),$("#dateNx5")]
+var tempature=[$("#tempature"),$("#tempNx1"),$("#tempNx2"),$("#tempNx3"),$("#tempNx4"),$("#tempNx5")];
+var windSpeed=[$("#windSpeed"),$("#windNx1"),$("#windNx2"),$("#windNx3"),$("#windNx4"),$("#windNx5")];
+var humidity=[$("#humidity"),$("#humidNx1"),$("#humidNx2"),$("#humidNx3"),$("#humidNx4"),$("#humidNx5")];
+
+
