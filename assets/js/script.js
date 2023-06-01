9145ca8bb66e509ca0422e6f5bff95a73722b94c
wygrajr <wycliffe.graftonjr@gmail.com>
2023-06-01 19:46:26 -0400

    Added javascript to call for weather. GETTING MASSIVE ERROR (index):6733 crbug/1173575, non-JS module files deprecated.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,56 @@ var tempature=[$("#tempature"),$("#tempNx1"),$("#tempNx2"),$("#tempNx3"),$("#tem
 var windSpeed=[$("#windSpeed"),$("#windNx1"),$("#windNx2"),$("#windNx3"),$("#windNx4"),$("#windNx5")];
 var humidity=[$("#humidity"),$("#humidNx1"),$("#humidNx2"),$("#humidNx3"),$("#humidNx4"),$("#humidNx5")];
 
+function getApi(requestUrl) {
+    fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        var geoLocationLat=data.city.coord.lat
+        var geoLocationLon=data.city.coord.lon
+        var requestWeatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${geoLocationLat}&lon=${geoLocationLon}&exclude=hourly,daily&units=imperial&appid=${key}`;
+        getWeatherAPI(requestWeatherUrl)
+      })
+};
+
+function getWeatherAPI (requestWeatherUrl){
+    fetch(requestWeatherUrl)
+        .then(function(response){
+          return response.json();
+        })
+        .then(function(data){
+
+            function getWeatherIcon(x) {
+                var todayIcon=data.list[x].weather[0].icon;
+                var img=$('<img>',{src:`https://openweathermap.org/img/wn/${todayIcon}@2x.png`})
+                return img
+            }
+
+            days[0].append(getWeatherIcon(0));
+            location[0].text(data.city.name+" - "+dateFormat(0));
+            tempature[0].text("Temp: "+data.list[0].main.temp+" F");
+            windSpeed[0].text("Wind: "+data.list[0].wind.speed+" mph");
+            humidity[0].text("Humidity: "+data.list[0].main.humidity+"%");
+
+            for (var i = 1, x = 6; i<6; i++, x=x+8){
+                days[i].append(getWeatherIcon(x));
+                location[i].text(dateFormat(x));
+                tempature[i].text("Temp: "+data.list[x].main.temp+" F");
+                windSpeed[i].text("Wind: "+data.list[x].wind.speed+" mph");
+                humidity[i].text("Humidity: "+data.list[x].main.humidity+"%");
+            }
+        })
+};
+
+function userSearch() {
+    cityName = $(search).val()
+    $(search).val("")
+};
+
+searchButton.on('click', function (event){
+    event.preventDefault();
+    userSearch();
+    var requestGeocodeUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${key}`;
+    getApi(requestGeocodeUrl);
+});
